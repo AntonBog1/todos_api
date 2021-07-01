f0a415b52503416eb9a6191602c01c825913bdfa
Anton <antonbogatirev1@gmail.com>
2021-06-30 22:46:02 -0700

    CRUD functionality
    Created multiple routes for the following.
    Showing all todos.
    Creating a new todo.
    Showing a todo based on its ID.
    Updating a todo based on its ID.
    Deleting a todo based on its ID.

--- routes/todos.js
+++ routes/todos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require("../models");
 
+// Show all todos 
 router.get('/', function(req, res){
     db.Todo.find()
     .then(function(todos){
@@ -12,10 +13,44 @@ router.get('/', function(req, res){
     })
 });
 
+// Create new todo
 router.post('/', function(req, res){
     db.Todo.create(req.body)
     .then(function(newTodo){
         res.status(201).json(newTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+});
+
+// Show todo based on the todo's ID
+router.get('/:todoId', function(req, res){
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+});
+
+// Update a todo
+router.put('/:todoId', function(req, res){
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+    .then(function(todo){
+        res.json(todo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+});
+
+// Delete a todo
+router.delete('/:todoId', function(req, res){
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: 'We deleted it!'})
     })
     .catch(function(err){
         res.send(err);
