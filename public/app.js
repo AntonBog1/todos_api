b8d89bf8ee97cfb63ebf345245b7df26700952c5
Anton <antonbogatirev1@gmail.com>
2021-07-01 22:27:42 -0700

    Displaying todos
    When the page loads all currently saved todos will load and populate as <li>s.
    Done todos will be displayed differently.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-// alert('test');
+$(document).ready(function () {
+    $.getJSON('/api/todos')
+        .then(addTodos);
+});
+
+function addTodos(todos) {
+    todos.forEach(function (todo) {
+        const newTodo = $('<li class="task">' + todo.name + '</li>')
+        if (todo.completed) {
+            newTodo.addClass('done');
+        }
+        $('.list').append(newTodo);
+    })
+};
