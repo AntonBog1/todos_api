9b4ed04c3a5136ea4eb0f68de73b936207d3c6ea
Anton <antonbogatirev1@gmail.com>
2021-07-03 09:58:56 -0700

    Toggling a todo as 'completed'
    The user can now mark a todo item as completed by clicking on it.

--- public/app.js
+++ public/app.js
@@ -8,7 +8,12 @@ $(document).ready(function () {
         }
     });
 
-    $('.list').on('click', 'span', function () {
+    $('.list').on('click', 'li', function () {
+        updateTodo($(this));
+    });
+
+    $('.list').on('click', 'span', function (e) {
+        e.stopPropagation();
         removeTodo($(this).parent());
     });
 });
@@ -22,6 +27,7 @@ function addTodos(todos) {
 function addTodo(todo) {
     const newTodo = $(`<li class="task">${todo.name}<span>X</span></li>`)
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if (todo.completed) {
         newTodo.addClass('done');
     }
@@ -49,7 +55,25 @@ function removeTodo(todo) {
         .then(function (data) {
             todo.remove();
         })
-        .catch(function (err){
-        console.log(err);
+        .catch(function (err) {
+            console.log(err);
+        })
+};
+
+function updateTodo(todo) {
+    const updateUrl = `/api/todos/${todo.data('id')}`;
+    const isDone = !todo.data('completed');
+    const updateData = { completed: isDone }
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
     })
+        .then(function (updatedTodo) {
+            todo.toggleClass('done');
+            todo.data('completed', isDone);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
 };
